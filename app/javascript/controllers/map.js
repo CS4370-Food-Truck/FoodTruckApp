accdce25b25cdbdf7864e2368398ff7107cfc734
jfelici4 <jfelici3@msudenver.edu>
2022-10-10 15:07:27 -0600

    Rename some variables
    Co-authored-by: Michelle <mrayne483@gmail.com>

--- app/javascript/controllers/map.js
+++ app/javascript/controllers/map.js
@@ -1,17 +1,14 @@
 import trucksJson from '/trucks.json' assert {type: 'json'};
 
-// Initialize and add the map
 function initMap() {
-    // The location of Uluru
-    const uluru = { lat: 39.743057, lng: -105.005554 };
-    // The map, centered at Uluru
+    const msu = { lat: 39.743057, lng: -105.005554 };
     const map = new google.maps.Map(document.getElementById("map"), {
         zoom: 16,
-        center: uluru,
+        center: msu,
     });
-    // The marker, positioned at Uluru
+
     const marker = new google.maps.Marker({
-        position: uluru,
+        position: msu,
         map: map,
     });
 }
