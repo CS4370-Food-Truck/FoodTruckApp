d8f64ed2608da96e0d953dcf43355a8e548088bc
Michelle Thornburgh <55558917+mrayne483@users.noreply.github.com>
2022-10-17 17:03:23 -0600

    Session control stub & sinup,mod'ed routes & table

--- config/routes.rb
+++ config/routes.rb
@@ -1,10 +1,22 @@
 Rails.application.routes.draw do
   resources :trucks
+  
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Defines the root path route ("/")
   root "map#index"
+
   get "/trucks-simple", to: "trucks#simple"
   get "/trucks-delete", to: "trucks#destroyall"
   get "/map", to: "map#index"
+
+  # User routes
+  resources :users, except: [:new]
+
+  # Session routes
+  get "signup", to: "users#new"
+  get "login", to: "sessions#new"
+  post "login", to: "sessions#create"
+  delete "logout", to: "sessions#destroy"
+  
 end
