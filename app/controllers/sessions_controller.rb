3566217247882ce4fdda7be124bae7340e9813fa
Michelle Thornburgh <55558917+mrayne483@users.noreply.github.com>
2022-10-20 16:30:54 -0600

    Working on the code for the login/out buttons

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -5,21 +5,20 @@ class SessionsController < ApplicationController
 
     def create
         user = User.find_by(username: params[:session][:username])
-        if user && user.authenticate(params[:session][:password])
+        if user !=nil && user.authenticate(params[:session][:password])
             session[:user_id] = user.id
             flash[:notice] = "Logged in successfully."
             redirect_to(trucks_path)
         else
-            #flash[:notice] = "There was something wrong with your login.  Please re-enter your login."
-            redirect_to(login_path)
-            #render('new')
+            flash[:notice] = "There was something wrong with your login.  Please re-enter your login."
+            render('new')
         end
     end
 
     def destroy
-        #session.delete(:current_user_id)
+        session.delete(:current_user_id)
         session[:user_id] = nil
-        #@_current_user = nil
+        @_current_user = nil
         flash[:notice] = "You are now logged out."
         redirect_to(trucks_path)
     end
