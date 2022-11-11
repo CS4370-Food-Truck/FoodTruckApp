759325c14c49da016ae4e712e7bedb8efe289ab2
jfelici4 <jfelici3@msudenver.edu>
2022-11-11 14:30:18 -0700

    Cleanup

--- public/service-worker.js
+++ public/service-worker.js
@@ -3,7 +3,7 @@ function onPush(event) {
 
     event.waitUntil(
         self.registration.showNotification(title, {
-            body: "We have received a push message",
+            body: "Trucks are arriving!",
             icon: "/assets/path/to/icon.png",
             tag:  "push-simple-demo-notification-tag"
         })
