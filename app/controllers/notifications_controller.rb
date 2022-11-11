dad30c520adab152089e077cca0b2cd70dbdb099
jfelici4 <jfelici3@msudenver.edu>
2022-11-11 14:49:50 -0700

    Notification testing on frontend

--- app/controllers/notifications_controller.rb
+++ app/controllers/notifications_controller.rb
@@ -48,23 +48,42 @@ class NotificationsController < ApplicationController
     redirect_to(trucks_path)
   end
   def notify
-    @subscription = PushSubscription.all
-    @subscription.each do |subscription|
+    #TODO: Remove this test for final release.
+    dayofweeknames = ["Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday"]
+    subscriptions = PushSubscription.all
+    truckstoday = ""
 
-      Webpush.payload_send(
-        message: "Testing notification",
-        endpoint: subscription.endpoint,
-        auth: subscription.keys['auth'],
-        p256dh: subscription.keys['p256dh'],
-        vapid: {
-          subject: "Subject.",
-          public_key: Figaro.env.NOTIFY_PUBLIC,
-          private_key: Figaro.env.NOTIFY_PRIVATE,
-          expiration:13.hours
-        }
-      )
+    subscriptions.each do |subscription|
+      begin
+        truckstocheck = subscription.trucks.split("  ")
+        truckstocheck.each do |truck|
+          truckid = truck.strip.to_i
+          entry = Truck.find(truckid)
+          schedule = JSON.parse(entry.schedule)
+          today = dayofweeknames[Date.today.wday]
+          if schedule[today] == true
+            truckstoday = truckstoday + entry.name + ", "
+          end
+        end
+      rescue ActiveRecord::RecordNotFound => e
+        puts('Invalid ID ignored.')
+      end
+
+      if truckstoday.length > 0
+        Webpush.payload_send(
+          message: "Food trucks are arriving today: " + truckstoday,
+          endpoint: subscription.endpoint,
+          auth: subscription.keys['auth'],
+          p256dh: subscription.keys['p256dh'],
+          vapid: {
+            subject: "Subject.",
+            public_key: Figaro.env.NOTIFY_PUBLIC,
+            private_key: Figaro.env.NOTIFY_PRIVATE,
+            expiration:13.hours
+          }
+        )
+      end
     end
     redirect_to(trucks_path)
   end
-
 end
