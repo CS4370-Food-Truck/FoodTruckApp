ca0d3e4042061267bdfd54a29989c4a3cbe91c4f
Arturo Valenzuela <arturovalemo@hotmail.com>
2022-11-18 16:12:50 -0700

    Removed unused code as well as pan to location feature.

--- app/javascript/controllers/map.js
+++ app/javascript/controllers/map.js
@@ -20,11 +20,6 @@ function weekDayToString(day){
     }
 }
 
-//don't worry about this, I might use it later, if not I'll delete - Arturo
-//infoWindow.setContent("<div>" + truck.name + "<br><input type='submit' id='butSubmit' value='Procurar' onclick='dicDic()'><div id='bar'></div></div>");
-
-
-
 function initMap() {
     const msu = {lat: 39.743057, lng: -105.005554};
     map = new google.maps.Map(document.getElementById("map"), {
@@ -92,72 +87,9 @@ function initMap() {
             InforObj.length = 0;
         }
     }
-
-    let infoWindow = new google.maps.InfoWindow();
-    const locationButton = document.createElement("button");
-
-    //adds button to find user location
-
-    locationButton.textContent = "Pan to Current Location";
-    locationButton.classList.add("custom-map-control-button");
-    map.controls[google.maps.ControlPosition.TOP_CENTER].push(locationButton);
-
-    function success(position){
-        const pos = {
-            lat: position.coords.latitude,
-            lng: position.coords.longitude,
-        };
-
-        infoWindow.setPosition(pos);
-        infoWindow.setContent("Location found.");
-        infoWindow.open(map);
-        map.setCenter(pos);
-        console.log(pos)
-    }
-
-    locationButton.addEventListener("click", () => {
-        // Try HTML5 geolocation.
-        if (navigator.geolocation) {
-            navigator.geolocation.getCurrentPosition(
-
-                (position) => {
-                    success(position)
-
-                },
-
-                () => {
-                    handleLocationError(true, infoWindow, map.getCenter());
-                }
-            );
-        } else {
-            // Browser doesn't support Geolocation
-            handleLocationError(false, infoWindow, map.getCenter());
-        }
-    });
-
-    /* Might use this later, will delete if I don't
-        //adds button for directions
-
-        const dicButton = document.createElement("button");
-        dicButton.textContent = "Directions";
-        dicButton.classList.add("custom-map-control-button");
-        map.controls[google.maps.ControlPosition.TOP_RIGHT].push(dicButton);
-
-        dicButton.addEventListener("click", () => {
-
-            console.log("hereeeeeeeeeeeee")
-            if (navigator.geolocation) {
-                navigator.geolocation.getCurrentPosition(locationSuccess, locationError);
-            }
-            else {
-                alert("Geolocation not supported");
-            }
-            console.log("now hereeeeeeeeeeeee")
-
-        });
-    */
 }
 
+//function gets user location and displays walking directions
 function getDirections(end) {
     console.log("position: " + end)
     if (navigator.geolocation) {
@@ -198,35 +130,5 @@ function getDirections(end) {
         handleLocationError(false, infoWindow, map.getCenter());
     }
 }
-
-/*
-function locationSuccess(position) {
-
-    var directionsService = new google.maps.DirectionsService();
-    var directionsDisplay = new google.maps.DirectionsRenderer();
-
-    var myLocation = new google.maps.LatLng(position.coords.latitude, position.coords.longitude);
-    var truckLocation = new google.maps.LatLng(39.741462, -105.009659);
-
-    directionsDisplay.setMap(map);
-
-    var request = {
-        origin: myLocation,
-        destination: truckLocation,
-        travelMode: 'WALKING'
-    };
-
-    directionsService.route(request, function (result, status) {
-        if (status == google.maps.DirectionsStatus.OK)
-            directionsDisplay.setDirections(result);
-    });
-
-}
-
-function locationError() {
-    alert("Couldn't get location");
-}
-*/
-
 
 window.initMap = initMap;
