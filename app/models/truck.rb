7a3b5c86c9edf9a1e3128723a58fd536398258ba
Juliet <JulietBLSmith@gmail.com>
2022-11-28 14:13:37 -0700

    still working. New issue array = nil

--- app/models/truck.rb
+++ app/models/truck.rb
@@ -1,44 +1,45 @@
 class Truck < ApplicationRecord
   validates :name, presence: true
   
-  before_save{|truck|
+  before_save do
+    self.schedule
     str = '{'
-    if truck.schedule.include?("Monday")
+    if self.schedule.include?("Monday")
       str += '"Monday": true,'
     else
       str += '"Monday": false,'
     end
-    if truck.schedule.include?("Tuesday")
+    if self.schedule.include?("Tuesday")
       str += '"Tuesday": true,'
     else
       str += '"Tuesday": false,'
     end
-    if truck.schedule.include?("Wednesday")
+    if self.schedule.include?("Wednesday")
       str += '"Wednesday": true,'
     else
       str += '"Wednesday": false,'
     end
-    if truck.schedule.include?("Thursday")
+    if self.schedule.include?("Thursday")
       str += '"Thursday": true,'
     else
       str += '"Thursday": false,'
     end
-    if truck.schedule.include?("Friday")
+    if self.schedule.include?("Friday")
       str += '"Friday": true,'
     else
       str += '"Friday": false,'
     end
-    if truck.schedule.include?("Saturday")
+    if self.schedule.include?("Saturday")
       str += '"Saturday": true,'
     else
       str += '"Saturday": false,'
     end
-    if truck.schedule.include?("Sunday")
+    if self.schedule.include?("Sunday")
       str += '"Sunday": true,'
     else
       str += '"Sunday": false,'
     end
     srt += '}'
-  truck.schedule = str
-  }
+    self.schedule = str
+  end
 end
