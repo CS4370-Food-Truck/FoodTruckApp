7d059c69dd3ddc3eea686ed9f09ad638173be32e
Juliet <JulietBLSmith@gmail.com>
2022-11-28 16:03:41 -0700

    documentations and some simple validations

--- app/models/truck.rb
+++ app/models/truck.rb
@@ -1,3 +1,11 @@
 class Truck < ApplicationRecord
+  # Truck validations before it saves to database
   validates :name, presence: true
+  validates :longitude, presence: true
+  validates :latitude, presence: true
+  validates :schedule, presence: true
+  validates :website, presence: true
+  validates :menu, presence: true
+  validates :longitude, numericality: {in: -104..-106}
+  validates :latitude, numericality: {in: 38..40}
 end
